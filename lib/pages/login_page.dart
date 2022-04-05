c31b67018dc61e9a8d062c1e6deb88a9eed8e95d
CRUXsur <marcojtorrico@hotmail.com>
2022-04-04 21:13:53 -0400

    Diseno del LoginPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,8 +4,91 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LoginPage'),
+      backgroundColor: const Color(0xffF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _Logo(),
+            _Form(),
+            _Labels(),
+            Text(
+              'Términos y condiciones de uso',
+              style: TextStyle(fontWeight: FontWeight.w200),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 170,
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage('assets/tag-logo.png')),
+            SizedBox(height: 20),
+            Text('Messenger', style: TextStyle(fontSize: 30))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  _Form({Key? key}) : super(key: key);
+
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          //
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  const _Labels({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text('No tienes cuenta?',
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300)),
+          SizedBox(height: 10),
+          GestureDetector(
+            child: Text('Crea una Ahora!',
+                style: TextStyle(
+                    color: Colors.blue[600],
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+            onTap: () {
+              //Navigator.pushReplacementNamed(context, this.ruta);
+            },
+          )
+        ],
       ),
     );
   }
