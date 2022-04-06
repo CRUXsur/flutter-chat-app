557ea66752fd40f4bb9dccfde2d8d232626c9674
CRUXsur <marcojtorrico@hotmail.com>
2022-04-05 22:21:09 -0400

    Registro y Navegar entre las pantallas

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,11 +1,96 @@
 import 'package:flutter/material.dart';
 
+import 'package:chat/widgets/custom_input.dart';
+import 'package:chat/widgets/logo.dart';
+import 'package:chat/widgets/labels.dart';
+import 'package:chat/widgets/boton_azul.dart';
+
 class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('RegisterPage'),
+      backgroundColor: const Color(0xffF2F2F2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          //* rebote para IOs y Android
+          physics: const BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Logo(titulo: 'Registro'),
+                _Form(),
+                Labels(
+                  ruta: 'login',
+                  titulo: '¿Ya tienes una cuenta?',
+                  subTitulo: 'Ingresa ahora!',
+                ),
+                Text(
+                  'Términos y condiciones de uso',
+                  style: TextStyle(fontWeight: FontWeight.w200),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form({Key? key}) : super(key: key);
+
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final nameCtrl = TextEditingController();
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          //
+          CustomInput(
+            icon: Icons.perm_identity,
+            placeholder: 'Nombre',
+            keyboardType: TextInputType.text,
+            textController: nameCtrl,
+          ),
+          //
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          //
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contrasena',
+            //borrar para que sea un texto normal
+            //keyboardType: TextInputType.emailAddress,
+            textController: passCtrl,
+            isPassword: true,
+          ),
+          //
+          BotonAzul(
+            text: 'Crear cuenta',
+            onPressed: () {
+              print(nameCtrl.text);
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            },
+          ),
+        ],
       ),
     );
   }
