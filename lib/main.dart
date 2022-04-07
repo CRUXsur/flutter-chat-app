7de5702462413bf4ab59d5bd89185cb71ddfb1cf
CRUXsur <marcojtorrico@hotmail.com>
2022-04-07 09:34:55 -0400

    ChatPage - Inicio

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       //persona tiene un token valido;
       // funciona    : va a usuarios
       // NO funciona : va a login
-      initialRoute: 'usuarios',
+      initialRoute: 'chat',
       routes: appRoutes,
     );
   }
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,9 +5,47 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: <Widget>[
+            CircleAvatar(
+              child: const Text('Te', style: TextStyle(fontSize: 12)),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+            ),
+            const SizedBox(height: 3),
+            const Text(
+              'Melissa Flores',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                //
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
+              ),
+            ),
+
+            const Divider(height: 1),
+
+            // TODO: Caja de texto
+            Container(
+              color: Colors.white,
+              height: 100,
+            )
+          ],
+        ),
       ),
     );
   }
